2516da7a15177abde3c58e5a82d89c20f1182726
Austin-AA <austin.aragon@wsu.edu>
2024-09-24 12:53:21 -0700

    added test proj

---
+++ exam1test/exam1test/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+using System.Runtime.Intrinsics.Arm;
+
+string s = "Hello World!";
+DoReplacement(ref s);
+Console.WriteLine(s);
+
+unsafe void DoReplacement(ref string s)
+{
+    s = s.Replace('H', 'J');
+    return;
+}
